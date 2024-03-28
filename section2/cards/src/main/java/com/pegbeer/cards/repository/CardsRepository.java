3abdd1d02249747733958d0052e13993693ecb7b
Pegbeer <55509957+Pegbeer@users.noreply.github.com>
2024-03-27 22:03:27 -0600

    loans & cards microservices

---
+++ section2/cards/src/main/java/com/pegbeer/cards/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.pegbeer.cards.repository;
+
+import com.pegbeer.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
